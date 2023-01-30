3e0e9b2379c5a89299aed0740947f18db8d1153f
Валерий Судаков <sudakov.valera@list.ru>
2023-01-30 21:30:09 +0300

    code refactor | added new constants | added day/night colors change | deleted PC folders

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -24,7 +24,6 @@ class _WeatherPageState extends State<WeatherPage> {
       setState(() {
         forecast = getWeather();
       });
-      print('timer worked');
     });
 
     super.initState();
